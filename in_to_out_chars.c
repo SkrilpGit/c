3feae94ae5c646242e7d698717ac08b2bc2354ed
SkrilpGit <jessica.smith@futuregen.net.au>
2026-03-08 14:03:01 +1100

    trying to figure out input and output, moved some things around

--- in_to_out_chars.c
+++ in_to_out_chars.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
 int main(){
+    int out[32];
+    int i = 0;
     int c;
     c = getchar();
     while(c != '\n'){
-        putchar(c);
+        //putchar(c);
+        out[i] = c;
         c = getchar();
+        i++;
     }
+    printf("\n buh: ");
+    for (int j =0; j <= i; j++)
+        printf("%c",out[j]);
     return 1;
 }
