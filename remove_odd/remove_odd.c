fc94039dfaac7ee98d869f99e52599c570aa0a62
SkrilpGit <jessica.smith@futuregen.net.au>
2026-08-26 16:14:40 +1000

    implemented the ability to define from an array

--- remove_odd/remove_odd.c
+++ remove_odd/remove_odd.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
-#include "int_list.h"
+#include "../intList/int_list.h"
 
 int main() {
-    struct IntList *list = int_list_create(4);
-    int value;
-    int_list_push_back(list,1);
-    int_list_push_back(list,2);
-    int_list_push_back(list,3);
-    int_list_push_back(list,4);
+    int arr[] = {1,2,3,4};
+    struct IntList *list = int_list_from(arr,4);
 
-    int len = 4;
+    int value;
 
     for (int i = 0; i < list->size; i++) {
         if (list->data[i] % 2 == 1){
             int_list_remove_at(list,i,&value);
-            len -= 1;
         }
     }
-    printf("size of list == %ld\n",list->size);
 
+    printf("size of list == %ld\nlist contains: [",list->size);
+    for (int i = 0; i < list->size; i++) {
+        int out; 
+        int_list_get(list,i,&out);
+        if (i == 0)
+            printf("%d",out);
+        else
+            printf(", %d",out);
+    }
+    printf("]\n");
+
+    return 0;
 }
 
